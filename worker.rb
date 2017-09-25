3990eadf76b5d999c967b80c7f8936f37d7a9785
Carlos Felipe Agudelo Ospina <cf.agudelo96@gmail.com>
2017-09-25 06:22:36 -0500

    Finally

--- worker.rb
+++ worker.rb
@@ -25,8 +25,6 @@ Dir.new(original_video_path).each do |movie_folder|
   end
 end
 
-puts converted_videos
-
 unless converted_videos.empty?
   HTTParty.post('http://ec2-34-229-67-157.compute-1.amazonaws.com/contests/videos_transcoded', body: {videos: converted_videos}.to_json, headers: { 'Content-Type' => 'application/json' })
 end
