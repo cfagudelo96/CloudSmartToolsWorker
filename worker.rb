528205f74b6f74b3fb52b4f8237997f0507c2f7e
Carlos Felipe Agudelo Ospina <cf.agudelo96@gmail.com>
2017-09-25 05:20:39 -0500

    p

--- worker.rb
+++ worker.rb
@@ -12,9 +12,9 @@ video_path = "#{base_path}/video"
 Dir.mkdir(original_video_path) unless Dir.exist?(original_video_path)
 Dir.mkdir(video_path) unless Dir.exist?(video_path)
 Dir.new(original_video_path).each do |movie_folder|
-  next if ignored_files.include?(movie_folder) || movie_folder.include?('CONVERTED')
+  next if ignored_files.include?(movie_folder)
   Dir.new("#{original_video_path}/#{movie_folder}").each do |movie_path|
-    next if ignored_files.include?(movie_path)
+    next if ignored_files.include?(movie_path) || movie_path.include?('converted')
     converted_folder_path = "#{video_path}/#{movie_folder}"
     Dir.mkdir(converted_folder_path) unless Dir.exist?(converted_folder_path)
     movie_file = File.new("#{converted_folder_path}/#{movie_folder}.mp4", 'w')
@@ -32,5 +32,7 @@ unless converted_videos.empty?
   http = Net::HTTP.new(uri.host, uri.port)
   request = Net::HTTP::Post.new(uri.request_uri, header)
   request.body = videos.to_json
-  http.request(request)
+  response = http.request(request)
+  puts response
+  puts response.body
 end
